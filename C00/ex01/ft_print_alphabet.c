8ab1396898e7aa42088e90a8a03f96ba0d56d808
jcclavel <jc.clavel@gmail.com>
2025-09-27 14:08:28 +0100

    Add files via upload

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jda-mota <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 16:19:57 by jda-mota          #+#    #+#             */
+/*   Updated: 2025/09/12 09:43:05 by jda-mota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	letter;
+
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		write (1, &letter, 1);
+		letter = letter +1;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	return (0);
+}*/
---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jda-mota <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/11 18:23:42 by jda-mota          #+#    #+#             */
+/*   Updated: 2025/09/11 19:45:51 by jda-mota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write (1, &letter, 1);
+		letter = letter -1;
+	}
+}
